8282c1ce25305337d7073ff842dde6261aa29b67
w.szutryk <w.szutryk@optimumpareto.com>
2023-03-18 14:54:11 +0100

    add async interactions helpers

---
+++ frontend/src/framework/asyncInteractions/frontifyRequestUrl.helper.ts
@@ -0,0 +1,8 @@
+export const FRONTIFY_URL = (
+  controlerUrl: string,
+  routeUrl?: string
+): string => {
+  const baseUrl = process.env.REACT_APP_API_URL || "no_API_URL_env_defined!";
+
+  return `${baseUrl}${controlerUrl}${routeUrl || ""}`;
+};
