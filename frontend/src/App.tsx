d46cbdd66919dd6fdd7f3490f2d1e4f5d29cba98
w.szutryk <w.szutryk@optimumpareto.com>
2023-03-18 14:52:15 +0100

    add basic routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,32 @@
 import PageTemplate from "atomicComponents/templates/PageTemplate";
-import { Suspense } from "react";
+import useSetInitialTheme from "framework/theme/useSetInitialTheme";
+import "framework/translations/config";
+import { TranslationKeys } from "framework/translations/translationKeys";
+import HomePage from "pages/HomePage";
+import { Suspense, useLayoutEffect } from "react";
+import { useTranslation } from "react-i18next";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import useSetInitialTheme from "theme/useSetInitialTheme";
 
 const App = (): JSX.Element => {
   useSetInitialTheme();
+
+  const { t } = useTranslation();
+
+  useLayoutEffect(() => {
+    const title = t(TranslationKeys.PageTitleMain);
+    document.title = title;
+  });
 
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<PageTemplate />}>
+          <Route path="/home" element={<PageTemplate />}>
             <Route
-              path="*"
+              index
               element={
                 <Suspense fallback={false}>
-                  <></>
+                  <HomePage />
                 </Suspense>
               }
             />
