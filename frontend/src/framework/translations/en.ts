1049c671646e5747e89349277ecc1ab791c8dd86
w.szutryk <w.szutryk@optimumparto.com>
2023-03-18 14:54:28 +0100

    add translations

---
+++ frontend/src/framework/translations/en.ts
@@ -0,0 +1,23 @@
+import { TranslationKeys } from "./translationKeys";
+
+export const enTranslation: Record<TranslationKeys, string> = {
+  [TranslationKeys.PageTitleMain]: "TodoList",
+
+  [TranslationKeys.PageTitleHome]: " - home",
+  [TranslationKeys.WelcomeTextHeader]: "TodoList",
+  [TranslationKeys.WelcomeTextDescription]:
+    "Welcome to our Todo/Reminder application! With our app, you can easily stay on top of your daily tasks and never forget an important appointment again. Our user-friendly interface and customizable features make it easy to create, organize, and prioritize your to-do lists and reminders. Get started today and take control of your busy schedule!",
+  [TranslationKeys.LoginPanelSeparatorText]: "or",
+  [TranslationKeys.GoToFeaturesButtonText]: "Explore features",
+  [TranslationKeys.LoginPanelHeader]: "Sign in or Sign up",
+  [TranslationKeys.LoginPanelHeaderDescription]:
+    "Sign in with google:",
+  [TranslationKeys.LoginPanelInputEmailLabel]: "email",
+  [TranslationKeys.LoginPanelInputPasswordLabel]: "password",
+  [TranslationKeys.LoginPanelInputPasswordRepeatLabel]: "password repeat",
+  [TranslationKeys.LoginButtonText]: "sign in",
+  [TranslationKeys.RegisterButtonText]: "sign up",
+  [TranslationKeys.GoogleSignIn]: "google",
+
+  [TranslationKeys.PageTitleFeatures]: " - funkcje",
+};
