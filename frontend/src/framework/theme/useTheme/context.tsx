12bdb8a9b8df1bf7dc2b89c0071402c39e5d8d67
w.szutryk <w.szutryk@optimumpareto.com>
2023-04-15 23:54:19 +0200

    theme refactor

---
+++ frontend/src/framework/theme/useTheme/context.tsx
@@ -0,0 +1,8 @@
+import { BaseContextProps } from "framework/contexts/base.context.props";
+import { createContext } from "react";
+import { ContextProps } from "./useCurrentUser.models";
+
+export const Context = createContext<BaseContextProps & ContextProps>({
+  changeTheme: () => {},
+  initialized: false,
+});
---
+++ frontend/src/framework/theme/useTheme/index.tsx
@@ -0,0 +1,62 @@
+import {
+  createTheme,
+  PaletteMode,
+  ThemeProvider as MuiThemeProvider,
+} from "@mui/material";
+import { ReactNode, useContext, useMemo, useState } from "react";
+import { TodoListTheme } from "../models/theme.const";
+import { darkTheme } from "../palletes/dark";
+import { lightTheme } from "../palletes/light";
+import { Context } from "./context";
+import { ContextProps } from "./useCurrentUser.models";
+
+interface Props {
+  children: ReactNode;
+}
+
+function ThemeProvider({ children }: Props): JSX.Element {
+  const [mode, setMode] = useState<PaletteMode>(
+    localStorage.getItem(TodoListTheme)
+      ? (localStorage.getItem(TodoListTheme) as PaletteMode)
+      : "light"
+  );
+  const muiTheme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: mode,
+          ...(mode === "dark" ? darkTheme : lightTheme),
+        },
+      }),
+    [mode]
+  );
+
+  const changeTheme = () => {
+    const newTheme = mode === "light" ? "dark" : "light";
+    setMode(newTheme);
+    localStorage.setItem(TodoListTheme, newTheme);
+  };
+
+  const value = {
+    changeTheme,
+    initialized: true,
+  };
+
+  return (
+    <Context.Provider value={value}>
+      <MuiThemeProvider theme={muiTheme}>{children}</MuiThemeProvider>
+    </Context.Provider>
+  );
+}
+
+function useTheme(): ContextProps {
+  const context = useContext(Context);
+
+  if (!context.initialized) {
+    throw new Error("Context must be used within a <ThemeProvider />");
+  }
+
+  return context;
+}
+
+export { ThemeProvider, useTheme };
