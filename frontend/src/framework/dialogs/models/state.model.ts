ade70250653c2f75e5d76df094ac3fd76b67092f
w.szutryk <w.szutryk@optimumparto.com>
2023-04-16 20:01:36 +0200

    add edit todoList muattaion

--- frontend/src/framework/dialogs/models/state.model.ts
+++ frontend/src/framework/dialogs/models/state.model.ts
@@ -1,7 +1,9 @@
+import { ITodoListAttached } from "linked-models/todoList/todoList.model";
 import { IBaseDialogProps } from "./baseModalProps.model";
 
 export interface ITodoListDialog extends IBaseDialogProps {
   visible: boolean;
+  editTodoListData?: ITodoListAttached;
 }
 
 export interface IDialogsState {
---
+++ frontend/src/pages/TodoListsPage/mutations/editTodoList.mutation.ts
@@ -0,0 +1,55 @@
+import { apiPut } from "framework/asyncInteractions";
+import { FRONTIFY_URL } from "framework/asyncInteractions/frontifyRequestUrl.helper";
+import { TodoListsWithTasksDto } from "linked-models/todoList/todoList.dto";
+import {
+  ITodoList,
+  ITodoListAttached,
+} from "linked-models/todoList/todoList.model";
+import {
+  PARAM_WITH_TASKS,
+  URL_TODO_LIST,
+  URL_TODO_LISTS,
+} from "linked-models/todoList/todoList.urls";
+import { useMutation, useQueryClient } from "react-query";
+
+export const useEditTodoListMutation = () => {
+  const queryClient = useQueryClient();
+
+  const editTodoList = async ({
+    todoListId,
+    data,
+  }: {
+    todoListId: string;
+    data: ITodoList;
+  }) => {
+    const url = FRONTIFY_URL(URL_TODO_LISTS, URL_TODO_LIST(todoListId));
+    return apiPut<ITodoList, ITodoListAttached>(url, data).then(
+      (res) => res.data
+    );
+  };
+
+  return useMutation(editTodoList, {
+    onSuccess: (updatedTodoList) => {
+      //update all todoLists query
+      queryClient.setQueryData(
+        [URL_TODO_LISTS, PARAM_WITH_TASKS],
+        (prev?: TodoListsWithTasksDto[]): TodoListsWithTasksDto[] => {
+          return (
+            prev?.map((td) => {
+              if (td.id === updatedTodoList.id) {
+                return { ...td, ...updatedTodoList };
+              }
+
+              return td;
+            }) || []
+          );
+        }
+      );
+      //update single query data
+      queryClient.setQueryData(
+        [URL_TODO_LISTS, URL_TODO_LIST(updatedTodoList.id)],
+        updatedTodoList
+      );
+    },
+  });
+};
