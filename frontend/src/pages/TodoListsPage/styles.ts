50c8e28e95cd96ad78f2da8bc87868bc4f40feb1
w.szutryk <w.szutryk@optimumparto.com>
2023-05-09 11:47:27 +0200

    start implementing drag and drop of TodoLists

--- frontend/src/pages/TodoListsPage/styles.ts
+++ frontend/src/pages/TodoListsPage/styles.ts
@@ -5,22 +5,29 @@ export const StyledTodoListsPageWrapper = styled("main")({
   top: 15,
 });
 
-export const StyledTodoListsWrapper = styled("section")(({ theme }) => ({
-  marginTop: theme.spacing(7),
+export const StyledTodoListsWrapper = styled("section", {
+  shouldForwardProp: (prop) => prop !== "columns",
+})<{ columns: number }>(({ theme, columns }) => ({
   display: "grid",
-  gridGap: 20,
-  gridTemplateColumns: "repeat(1, 1fr)",
-  gridTemplateRows: "auto",
-  [theme.breakpoints.up("sm")]: {
-    gridTemplateColumns: "repeat(2, 1fr)",
-    "& > button": {
-      marginTop: 45,
-    },
-  },
-  [theme.breakpoints.up("lg")]: {
-    gridTemplateColumns: "repeat(3, 1fr)",
-  },
-  "& > button": {
-    marginTop: 45,
-  },
+  gridTemplateColumns: `repeat(${columns}, 1fr)`,
+  gridGap: 10,
+  maxWidth: "800px",
+  margin: "100px auto",
+  // marginTop: theme.spacing(7),
+  // display: "grid",
+  // gridGap: 20,
+  // gridTemplateColumns: "repeat(1, 1fr)",
+  // gridTemplateRows: "auto",
+  // [theme.breakpoints.up("sm")]: {
+  //   gridTemplateColumns: "repeat(2, 1fr)",
+  //   "& > button": {
+  //     marginTop: 45,
+  //   },
+  // },
+  // [theme.breakpoints.up("lg")]: {
+  //   gridTemplateColumns: "repeat(3, 1fr)",
+  // },
+  // "& > button": {
+  //   marginTop: 45,
+  // },
 }));
