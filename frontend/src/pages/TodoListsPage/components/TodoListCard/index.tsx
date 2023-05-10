262d572738f8bf62c1c01095e73b9accbbdfece4
w.szutryk <w.szutryk@optimumpareto.com>
2023-05-10 13:15:19 +0200

    fix sortable todoLists styles

--- frontend/src/pages/TodoListsPage/components/TodoListCard/index.tsx
+++ frontend/src/pages/TodoListsPage/components/TodoListCard/index.tsx
@@ -1,9 +1,14 @@
+import {
+  AnimateLayoutChanges,
+  defaultAnimateLayoutChanges,
+  useSortable,
+} from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import ShareIcon from "@mui/icons-material/Share";
 import { AvatarGroup } from "@mui/material";
-import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardHeader from "@mui/material/CardHeader";
@@ -15,10 +20,26 @@ import UserAvatar from "atomicComponents/molecules/UserAvatar";
 import { IExtendedTodoListDto } from "linked-models/todoList/todoList.dto";
 import * as React from "react";
 import { memo } from "react";
-import { StyledExpandMore } from "./styles";
+import {
+  StyledDragIcon,
+  StyledExpandMore,
+  StyledSortableItem,
+  StyledTodoListCard,
+} from "./styles";
+
+const animateLayoutChanges: AnimateLayoutChanges = (args) => {
+  const { isSorting, wasDragging } = args;
+
+  if (isSorting || wasDragging) {
+    return defaultAnimateLayoutChanges(args);
+  }
+
+  return true;
+};
 
 interface Props {
   todoList: IExtendedTodoListDto;
+  withShakeAnimation?: boolean;
 }
 
 const TodoListCard = ({
@@ -32,8 +53,17 @@ const TodoListCard = ({
     assignedOwners,
     assignedUsers,
   },
+  withShakeAnimation,
 }: Props): JSX.Element => {
   const [expanded, setExpanded] = React.useState(false);
+  const {
+    isDragging,
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+  } = useSortable({ id, animateLayoutChanges });
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
@@ -44,79 +74,92 @@ const TodoListCard = ({
   if (assignedUsers) allMembers.push(...assignedUsers);
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardHeader
-        avatar={
-          <AvatarGroup max={3}>
-            {Array.from(new Set(allMembers)).map((user) => (
-              <UserAvatar key={user.id} userId={user.id} />
-            ))}
-          </AvatarGroup>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
-      />
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the
-          mussels, if you like.
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <Button>Dodaj task</Button>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <StyledExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </StyledExpandMore>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
+    <StyledSortableItem
+      isDragging={isDragging}
+      ref={setNodeRef}
+      transition={transition}
+      transform={CSS.Transform.toString(transform)}
+    >
+      <StyledTodoListCard withShakeAnimation={withShakeAnimation}>
+        <CardHeader
+          avatar={
+            <AvatarGroup max={3}>
+              {Array.from(new Set(allMembers)).map((user) => (
+                <UserAvatar key={user.id} userId={user.id} />
+              ))}
+            </AvatarGroup>
+          }
+          action={
+            <>
+              <MoreVertIcon />
+              <StyledDragIcon
+                {...listeners}
+                {...attributes}
+                isDragging={isDragging}
+              />
+            </>
+          }
+          title="Shrimp and Chorizo Paella"
+          subheader="September 14, 2016"
+        />
         <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and
-            set aside for 10 minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
-            over medium-high heat. Add chicken, shrimp and chorizo, and cook,
-            stirring occasionally until lightly browned, 6 to 8 minutes.
-            Transfer shrimp to a large plate and set aside, leaving chicken and
-            chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes,
-            onion, salt and pepper, and cook, stirring often until thickened and
-            fragrant, about 10 minutes. Add saffron broth and remaining 4 1/2
-            cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is
-            absorbed, 15 to 18 minutes. Reduce heat to medium-low, add reserved
-            shrimp and mussels, tucking them down into the rice, and cook again
-            without stirring, until mussels have opened and rice is just tender,
-            5 to 7 minutes more. (Discard any mussels that don&apos;t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then
-            serve.
+          <Typography variant="body2" color="text.secondary">
+            This impressive paella is a perfect party dish and a fun meal to
+            cook together with your guests. Add 1 cup of frozen peas along with
+            the mussels, if you like.
           </Typography>
         </CardContent>
-      </Collapse>
-    </Card>
+        <CardActions disableSpacing>
+          <Button>Dodaj task</Button>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="share">
+            <ShareIcon />
+          </IconButton>
+          <StyledExpandMore
+            expand={expanded}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </StyledExpandMore>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph>Method:</Typography>
+            <Typography paragraph>
+              Heat 1/2 cup of the broth in a pot until simmering, add saffron
+              and set aside for 10 minutes.
+            </Typography>
+            <Typography paragraph>
+              Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
+              over medium-high heat. Add chicken, shrimp and chorizo, and cook,
+              stirring occasionally until lightly browned, 6 to 8 minutes.
+              Transfer shrimp to a large plate and set aside, leaving chicken
+              and chorizo in the pan. Add pimentón, bay leaves, garlic,
+              tomatoes, onion, salt and pepper, and cook, stirring often until
+              thickened and fragrant, about 10 minutes. Add saffron broth and
+              remaining 4 1/2 cups chicken broth; bring to a boil.
+            </Typography>
+            <Typography paragraph>
+              Add rice and stir very gently to distribute. Top with artichokes
+              and peppers, and cook without stirring, until most of the liquid
+              is absorbed, 15 to 18 minutes. Reduce heat to medium-low, add
+              reserved shrimp and mussels, tucking them down into the rice, and
+              cook again without stirring, until mussels have opened and rice is
+              just tender, 5 to 7 minutes more. (Discard any mussels that
+              don&apos;t open.)
+            </Typography>
+            <Typography>
+              Set aside off of the heat to let rest for 10 minutes, and then
+              serve.
+            </Typography>
+          </CardContent>
+        </Collapse>
+      </StyledTodoListCard>
+    </StyledSortableItem>
   );
 };
 
--- frontend/src/pages/TodoListsPage/components/TodoListCard/styles.ts
+++ frontend/src/pages/TodoListsPage/components/TodoListCard/styles.ts
@@ -1,7 +1,49 @@
-import { styled } from "@mui/material";
+import DragIndicatorIcon from "@mui/icons-material/DragIndicator";
+import { Card, styled } from "@mui/material";
+
+export const StyledSortableItem = styled("div", {
+  shouldForwardProp: (prop) =>
+    prop !== "isDragging" && prop !== "transform" && prop !== "transition",
+})<{
+  isDragging: boolean;
+  transform: string | undefined;
+  transition: string | undefined;
+}>(({ isDragging, transform, transition }) => ({
+  opacity: isDragging ? "0.5" : "1",
+  transformOrigin: "50% 50%",
+  boxShadow: isDragging
+    ? "rgb(63 63 68 / 5%) 0px 2px 0px 2px, rgb(34 33 81 / 15%) 0px 2px 3px 2px"
+    : "rgb(63 63 68 / 5%) 0px 0px 0px 1px, rgb(34 33 81 / 15%) 0px 1px 3px 0px",
+  transform,
+  transition: transition || undefined,
+}));
+
+export const StyledTodoListCard = styled(Card, {
+  shouldForwardProp: (prop) => prop !== "withShakeAnimation",
+})<{
+  withShakeAnimation?: boolean;
+}>(({ withShakeAnimation }) => ({
+  "@keyframes shake": {
+    "0%": { transform: "rotate(0deg)" },
+    "25%": { transform: "rotate(-1deg)" },
+    "75%": { transform: "rotate(1deg)" },
+    "100%": { transform: "rotate(0deg)" },
+  },
+  animation: withShakeAnimation ? "shake 1s" : "unset",
+  animationIterationCount: "infinite",
+  maxWidth: 345,
+}));
+
+export const StyledDragIcon = styled(DragIndicatorIcon, {
+  shouldForwardProp: (prop) => prop !== "isDragging",
+})<{ isDragging: boolean }>(({ isDragging }) => ({
+  outline: "none",
+  border: "none",
+  cursor: isDragging ? "grabbing" : "grab",
+}));
 
 export const StyledExpandMore = styled("div", {
-  shouldForwardProp: (prop) => prop !== "isAuth",
+  shouldForwardProp: (prop) => prop !== "expand",
 })<{ expand: boolean }>(({ theme, expand }) => ({
   cursor: "pointer",
   transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
