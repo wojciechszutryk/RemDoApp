1974714306a6d620eb63c64b8f75de911ab50395
w.szutryk <w.szutryk@optimumparto.com>
2023-05-24 18:08:54 +0200

    change images to svg, change information template

--- frontend/src/framework/dialogs/components/DeleteTodoListModal/index.tsx
+++ frontend/src/framework/dialogs/components/DeleteTodoListModal/index.tsx
@@ -1,4 +1,5 @@
 import Dialog from "atomicComponents/atoms/Dialog";
+import { BoatImage } from "atomicComponents/atoms/images/Boat";
 import InformationTemplate from "atomicComponents/molecules/InformationTemplate";
 import { useDialogs } from "framework/dialogs";
 import { initialDeleteTodoListDialog } from "framework/dialogs/models/initialState.const";
@@ -30,26 +31,12 @@ const DeleteTodoListModal = (): JSX.Element => {
       onClose={() => updateDeleteTodoListDialog(initialDeleteTodoListDialog)}
     >
       <InformationTemplate
-        headerStylesOverride={{
-          fontSize: { xs: 20, md: 32 },
-          textAlign: "center",
-          width: { xs: "95%", md: "40%", xl: "30%" },
-          margin: "0 auto 58px",
-        }}
-        imageStylesOverride={{
-          display: "block",
-          margin: "8px auto 0",
-          width: { xs: 186, md: 295, xl: 397 },
-          height: { xs: 185, md: 295, xl: 397 },
-        }}
-        imageSrc={`${process.env.REACT_APP_URL}/images/boat.svg`}
-        imageAlt={"delete"}
+        image={<BoatImage />}
         headerText={t(TranslationKeys.DelteTodoListWarning)}
         actionButton={{
           children: t(TranslationKeys.DelteTodoList),
           onClick: onDelete,
         }}
-        reversed
       />
     </Dialog>
   );
