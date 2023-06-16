44780d45b5d1f1a644f110b4a858a565b84f8594
w.szutryk <w.szutryk@optimumpareto.com>
2023-06-16 14:28:14 +0200

    clean notifications menu & add hooks

---
+++ frontend/src/atomicComponents/organisms/Header/components/NotificationsMenu/hooks/useNotificationsData.ts
@@ -0,0 +1,64 @@
+import { useCurrentUser } from "framework/authentication/useCurrentUser";
+import { TranslationKeys } from "framework/translations/translatedTexts/translationKeys";
+import { INotificationDto } from "linked-models/notification/notification.dto";
+import { NotificationState } from "linked-models/notification/notification.enum";
+import { useMemo } from "react";
+import { useTranslation } from "react-i18next";
+
+const useNotificationsData = () => {
+  const { t } = useTranslation();
+  const { notifications } = useCurrentUser();
+
+  return useMemo(() => {
+    const readNotificationIDs: string[] = [];
+    const freahNotificationIDs: string[] = [];
+    const archivedNotificationIDs: string[] = [];
+    const todoListIdToActiveNotificationsMap = new Map<
+      string,
+      INotificationDto[]
+    >();
+    const todoListIdToArchivedNotificationsMap = new Map<
+      string,
+      INotificationDto[]
+    >();
+
+    notifications.forEach((n) => {
+      if (n.state === NotificationState.Archived) {
+        archivedNotificationIDs.push(n.userNotificationId);
+        const todoListId = n.todoListId || t(TranslationKeys.Other);
+        const notificationsForTodoList =
+          todoListIdToArchivedNotificationsMap.get(todoListId);
+
+        if (notificationsForTodoList) {
+          notificationsForTodoList.push(n);
+        } else {
+          todoListIdToArchivedNotificationsMap.set(todoListId, [n]);
+        }
+      } else {
+        if (n.state === NotificationState.Read)
+          readNotificationIDs.push(n.userNotificationId);
+        else freahNotificationIDs.push(n.userNotificationId);
+
+        const todoListId = n.todoListId || t(TranslationKeys.Other);
+        const notificationsForTodoList =
+          todoListIdToActiveNotificationsMap.get(todoListId);
+
+        if (notificationsForTodoList) {
+          notificationsForTodoList.push(n);
+        } else {
+          todoListIdToActiveNotificationsMap.set(todoListId, [n]);
+        }
+      }
+    });
+
+    return {
+      todoListIdToArchivedNotificationsMap,
+      archivedNotificationIDs,
+      todoListIdToActiveNotificationsMap,
+      readNotificationIDs,
+      freahNotificationIDs,
+    };
+  }, [notifications, t]);
+};
+
+export default useNotificationsData;
--- frontend/src/atomicComponents/organisms/Header/components/NotificationsMenu/index.tsx
+++ frontend/src/atomicComponents/organisms/Header/components/NotificationsMenu/index.tsx
@@ -1,87 +1,36 @@
-import ArchiveIcon from "@mui/icons-material/Archive";
-import CircleIcon from "@mui/icons-material/Circle";
-import DeleteIcon from "@mui/icons-material/Delete";
 import NotificationsIcon from "@mui/icons-material/Notifications";
-import {
-  Divider,
-  Drawer,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  ListSubheader,
-  useTheme,
-} from "@mui/material";
-import SwippableItem from "atomicComponents/molecules/SwippableItem";
-import { useCurrentUser } from "framework/authentication/useCurrentUser";
-import { TranslationKeys } from "framework/translations/translatedTexts/translationKeys";
-import { INotificationDto } from "linked-models/notification/notification.dto";
-import { NotificationState } from "linked-models/notification/notification.enum";
+import { Divider, Drawer } from "@mui/material";
 import { IExtendedTodoListDto } from "linked-models/todoList/todoList.dto";
-import TodoListIcon from "pages/TodoListsPage/components/TodoListIcon";
 import { useGetUserExtendedTodoListsQuery } from "pages/TodoListsPage/queries/getUserExtendedTodoLists.query";
 import { memo, useMemo, useState } from "react";
-import { useTranslation } from "react-i18next";
-import { useDeleteUserNotificationsMutation } from "../../mutations/deleteUserNotification.mutation";
-import { useEditUserNotificationsMutation } from "../../mutations/editUserNotification.mutation";
 import { StyledHeaderButton } from "../../styles";
+import ArchivedNotificationsTopPanel from "./components/ArchivedNotificationsTopPanel";
+import EmptyNotificationsInfo from "./components/EmptyNotificationsInfo";
+import NewNotificationsTopPanel from "./components/NewNotificationsTopPanel";
+import NotificationsList from "./components/NotificationsList";
+import useMarkFreshNotificationsAsRead from "./hooks/useMarkFreshNotificationsAsRead";
+import useNotificationsData from "./hooks/useNotificationsData";
+import useToggleDrawer from "./hooks/useToggleDrawer";
 import { StyledDrawerListWrapper } from "./styles";
 
 const NotificationsMenu = (): JSX.Element => {
   const [showNotificationDrawer, setShowNotificationDrawer] = useState(false);
-  const { notifications } = useCurrentUser();
-  const editUserNotificationMutation = useEditUserNotificationsMutation();
-  const deleteUserNotificationsMutation = useDeleteUserNotificationsMutation();
+
   const getUserTodoListsWithTasksQuery = useGetUserExtendedTodoListsQuery();
-  const { t } = useTranslation();
+  const {
+    archivedNotificationIDs,
+    readNotificationIDs,
+    freahNotificationIDs,
+    todoListIdToActiveNotificationsMap,
+    todoListIdToArchivedNotificationsMap,
+  } = useNotificationsData();
 
-  //todo: change fresh to read after 3 seconds
-  //todo: modify archived notifications
-
-  const toggleDrawer =
-    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
-      if (
-        event.type === "keydown" &&
-        ((event as React.KeyboardEvent).key === "Tab" ||
-          (event as React.KeyboardEvent).key === "Shift")
-      ) {
-        return;
-      }
-
-      setShowNotificationDrawer(open);
-    };
-
-  const theme = useTheme();
-
-  const [archivedNotifications, activeNotifications] = useMemo(() => {
-    const activeNotifications = new Map<string, INotificationDto[]>();
-    const archivedNotifications = new Map<string, INotificationDto[]>();
-
-    notifications.forEach((n) => {
-      if (n.state === NotificationState.Archived) {
-        const todoListId = n.todoListId || t(TranslationKeys.Other);
-        const notificationsForTodoList = archivedNotifications.get(todoListId);
-
-        if (notificationsForTodoList) {
-          notificationsForTodoList.push(n);
-        } else {
-          archivedNotifications.set(todoListId, [n]);
-        }
-      } else {
-        const todoListId = n.todoListId || t(TranslationKeys.Other);
-        const notificationsForTodoList = activeNotifications.get(todoListId);
-
-        if (notificationsForTodoList) {
-          notificationsForTodoList.push(n);
-        } else {
-          activeNotifications.set(todoListId, [n]);
-        }
-      }
-    });
-
-    return [archivedNotifications, activeNotifications];
-  }, [notifications, t]);
+  const markFreshNotificationsAsRead =
+    useMarkFreshNotificationsAsRead(freahNotificationIDs);
+  const toggleDrawer = useToggleDrawer({
+    setDrawerState: setShowNotificationDrawer,
+    onClose: freahNotificationIDs ? markFreshNotificationsAsRead : undefined,
+  });
 
   const todoListsMap = useMemo(() => {
     const todoListsMap = new Map<string, IExtendedTodoListDto>();
@@ -99,118 +48,48 @@ const NotificationsMenu = (): JSX.Element => {
         {<NotificationsIcon />}
       </StyledHeaderButton>
       <Drawer open={showNotificationDrawer} onClose={toggleDrawer(false)}>
+        /** TODO: loader while mutating */
         <StyledDrawerListWrapper
           role="presentation"
           onClick={toggleDrawer(false)}
           onKeyDown={toggleDrawer(false)}
         >
-          <List>
-            {Array.from(activeNotifications.entries()).map(
-              ([todoListId, notifications]) => {
-                const headerIcon = todoListsMap.get(todoListId)?.icon;
-                return (
-                  <li key={todoListId}>
-                    <ul>
-                      <ListSubheader>
-                        {headerIcon && (
-                          <ListItemIcon>
-                            <TodoListIcon type={headerIcon} disableHover />
-                          </ListItemIcon>
-                        )}
-                        {todoListsMap.get(todoListId)?.name}
-                      </ListSubheader>
-                      {notifications.map(
-                        ({ userNotificationId, state, message }) => {
-                          const isFresh = state === NotificationState.Fresh;
-                          return (
-                            <SwippableItem
-                              key={userNotificationId}
-                              defaultColor={theme.palette.info.main}
-                              rightShift={{
-                                color: theme.palette.background.paper,
-                                Icon: <ArchiveIcon />,
-                                action: () =>
-                                  deleteUserNotificationsMutation.mutate([
-                                    userNotificationId,
-                                  ]),
-                              }}
-                              leftShift={{
-                                color: theme.palette.warning.main,
-                                Icon: <DeleteIcon />,
-                                action: () =>
-                                  editUserNotificationMutation.mutate([
-                                    {
-                                      editedUserNotificationId:
-                                        userNotificationId,
-                                      state: NotificationState.Archived,
-                                    },
-                                  ]),
-                              }}
-                            >
-                              <ListItem
-                                disablePadding
-                                onClick={(e) => {
-                                  e.stopPropagation();
-                                }}
-                              >
-                                <ListItemButton>
-                                  {isFresh && (
-                                    <ListItemIcon>
-                                      <CircleIcon />
-                                    </ListItemIcon>
-                                  )}
-                                  <ListItemText primary={message} />
-                                </ListItemButton>
-                              </ListItem>
-                            </SwippableItem>
-                          );
-                        }
-                      )}
-                    </ul>
-                  </li>
-                );
-              }
-            )}
-          </List>
-          <Divider />
-          <List>
-            {Array.from(archivedNotifications.entries()).map(
-              ([todoListId, notifications]) => {
-                const headerIcon = todoListsMap.get(todoListId)?.icon;
-                return (
-                  <li key={todoListId}>
-                    <ul>
-                      <ListSubheader>
-                        {headerIcon && (
-                          <ListItemIcon>
-                            <TodoListIcon type={headerIcon} disableHover />
-                          </ListItemIcon>
-                        )}
-                        {todoListsMap.get(todoListId)?.name}
-                      </ListSubheader>
-                      {notifications.map(
-                        ({ userNotificationId, message }) => {
-                          return (
-                            <ListItem
-                              key={userNotificationId}
-                              disablePadding
-                              onClick={(e) => {
-                                e.stopPropagation();
-                              }}
-                            >
-                              <ListItemButton>
-                                <ListItemText primary={message} />
-                              </ListItemButton>
-                            </ListItem>
-                          );
-                        }
-                      )}
-                    </ul>
-                  </li>
-                );
-              }
-            )}
-          </List>
+          {archivedNotificationIDs.length <= 0 ? (
+            <EmptyNotificationsInfo />
+          ) : (
+            <>
+              <NewNotificationsTopPanel
+                notificationIDs={[
+                  ...freahNotificationIDs,
+                  ...readNotificationIDs,
+                ]}
+              />
+              <NotificationsList
+                todoListIdToNotificationsMap={
+                  todoListIdToActiveNotificationsMap
+                }
+                todoListsMap={todoListsMap}
+                rightShiftAction="archive"
+              />
+              <Divider />
+            </>
+          )}
+          {archivedNotificationIDs.length <= 0 ? (
+            <EmptyNotificationsInfo archivedList />
+          ) : (
+            <>
+              <ArchivedNotificationsTopPanel
+                notificationIDs={archivedNotificationIDs}
+              />
+              <NotificationsList
+                todoListIdToNotificationsMap={
+                  todoListIdToArchivedNotificationsMap
+                }
+                todoListsMap={todoListsMap}
+                rightShiftAction="unarchive"
+              />
+            </>
+          )}
         </StyledDrawerListWrapper>
       </Drawer>
     </>
