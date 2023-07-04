2c767b85d109f598e6fb4981342dbf86170f3a1c
w.szutryk <w.szutryk@optimumparto.com>
2023-07-04 16:40:11 +0200

    change FRONTIFY_URL to use URLSearchParams

--- frontend/src/framework/asyncInteractions/frontifyRequestUrl.helper.ts
+++ frontend/src/framework/asyncInteractions/frontifyRequestUrl.helper.ts
@@ -1,8 +1,11 @@
 export const FRONTIFY_URL = (
   controlerUrl: string,
-  routeUrl?: string
+  routeUrl?: string,
+  queryParams?: Record<string, string>,
 ): string => {
-  const baseUrl = process.env.REACT_APP_API_URL || "no_API_URL_env_defined!";
+  const baseUrl = process.env.REACT_APP_API_URL || 'no_API_URL_env_defined!';
 
-  return `${baseUrl}${controlerUrl}${routeUrl || ""}`;
+  return `${baseUrl}${controlerUrl}${routeUrl || ''}${
+    queryParams ? `?${new URLSearchParams(queryParams).toString()}` : ''
+  }`;
 };
