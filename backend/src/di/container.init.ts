ac62f7b6c8e83f64dc47e13320f1be83dd74bd2f
w.szutryk <w.szutryk@optimumpareto.com>
2023-07-27 11:53:22 +0200

    controllers structure refactor on BE

--- backend/src/di/container.init.ts
+++ backend/src/di/container.init.ts
@@ -1,10 +1,10 @@
-import "controllers/todoList/todoList.controller";
-import "controllers/todoList/todoList.task.controller";
-import "controllers/todoList/todoList.tasks.controller";
-import "controllers/todoList/todoLists.controller";
-import "controllers/user/user.auth.controller";
-import "controllers/user/user.controller";
-import "controllers/user/user.notification";
+import "controllers/todoList.controller";
+import "controllers/todoList.task.controller";
+import "controllers/todoList.tasks.controller";
+import "controllers/todoLists.controller";
+import "controllers/user.auth.controller";
+import "controllers/user.controller";
+import "controllers/user.notification";
 import { Container } from "inversify";
 
 export const container = new Container({
