f7326a4646e8f1abbcf25a026a761305376292fd
w.szutryk <w.szutryk@optimumpareto.com>
2023-07-27 12:00:18 +0200

    start adding reminders controller on BE

---
+++ backend/src/controllers/reminders.controller.ts
@@ -0,0 +1,76 @@
+import { currentUser } from "decorators/currentUser.decorator";
+import { inject } from "inversify";
+import {
+  BaseHttpController,
+  controller,
+  httpGet,
+  httpPost,
+  queryParam,
+  requestBody,
+} from "inversify-express-utils";
+import { OkResult } from "inversify-express-utils/lib/results";
+import { URL_REMINDERS } from "linked-models/reminder/reminder.urls";
+import { ITodoList } from "linked-models/todoList/todoList.model";
+import {
+  PARAM_EXTENDED,
+  PARAM_WITH_MEMBERS,
+} from "linked-models/todoList/todoList.urls";
+import { IUserAttached } from "linked-models/user/user.model";
+import { SetCurrentUser } from "middlewares/user/setCurrentUser.middleware";
+import { TodoListCacheService } from "services/todoList/todoList.cache.service";
+import { TodoListService } from "services/todoList/todoList.service";
+
+@controller(URL_REMINDERS, SetCurrentUser)
+export class TodoListsController extends BaseHttpController {
+  constructor(
+    @inject(TodoListService) private readonly todoListService: TodoListService,
+    @inject(TodoListCacheService)
+    private readonly todoListCacheService: TodoListCacheService
+  ) {
+    super();
+  }
+
+  @httpGet("")
+  async getTodoListsForUser(
+    @currentUser() currentUser: IUserAttached,
+    @queryParam(PARAM_EXTENDED) extended = false,
+    @queryParam(PARAM_WITH_MEMBERS) withMembers = false
+  ): Promise<OkResult> {
+    if (extended) {
+      const todoLists =
+        await this.todoListCacheService.getCachedExtendedTodoListsForUser(
+          currentUser.id
+        );
+
+      return this.ok(todoLists);
+    } else if (withMembers) {
+      const { todoLists } =
+        await this.todoListService.getTodoListsWithMembersForUser(
+          currentUser.id
+        );
+
+      return this.ok(todoLists);
+    }
+
+    const todoLists = await this.todoListService.getTodoListsForUser(
+      currentUser.id
+    );
+
+    return this.ok(todoLists);
+  }
+
+  @httpPost("")
+  async createTodoList(
+    @currentUser() currentUser: IUserAttached,
+    @requestBody() body: ITodoList
+  ): Promise<OkResult> {
+    if (!body.name) return this.json("Invalid data", 400);
+
+    const todoList = await this.todoListService.createTodoList(
+      body,
+      currentUser.id
+    );
+
+    return this.ok(todoList);
+  }
+}
