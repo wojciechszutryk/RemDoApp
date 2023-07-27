66c32f93f94bcfcdaf32a1a800f978c2955beadb
w.szutryk <w.szutryk@optimumpareto.com>
2023-07-27 13:01:46 +0200

    chenge reminders query to use IReminderDTO Interface

--- frontend/src/pages/RemindersPage/queries/getUserRemindersForDateRange.query.ts
+++ frontend/src/pages/RemindersPage/queries/getUserRemindersForDateRange.query.ts
@@ -1,12 +1,12 @@
 import { useQuery, UseQueryOptions } from "@tanstack/react-query";
 import { apiGet } from "framework/asyncInteractions";
 import { FRONTIFY_URL } from "framework/asyncInteractions/frontifyRequestUrl.helper";
+import { IReminderDTO } from "linked-models/reminder/reminder.dto";
 import {
   PARAM_END_DATE,
   PARAM_START_DATE,
   URL_REMINDERS,
 } from "linked-models/reminder/reminder.urls";
-import { IExtendedTodoListDto } from "linked-models/todoList/todoList.dto";
 import {
   PARAM_EXTENDED,
   URL_TODO_LISTS,
@@ -15,7 +15,7 @@ import { DateRange } from "react-big-calendar";
 
 export const useGetUserRemindersForDateRange = (
   dateRange: DateRange,
-  options?: Omit<UseQueryOptions<IExtendedTodoListDto[]>, "queryFn">
+  options?: Omit<UseQueryOptions<IReminderDTO[]>, "queryFn">
 ) => {
   const url = FRONTIFY_URL(URL_REMINDERS, "", {
     [PARAM_START_DATE]: encodeURIComponent(dateRange.start.toString()),
@@ -23,7 +23,7 @@ export const useGetUserRemindersForDateRange = (
   });
 
   const getTodoListsWithTasks = async () => {
-    return await apiGet<IExtendedTodoListDto[]>(url).then((res) => res.data);
+    return await apiGet<IReminderDTO[]>(url).then((res) => res.data);
   };
 
   return useQuery(
