d1709bb2d69cbe6a68b8f9b6727d69985c6df2a1
w.szutryk <w.szutryk@optimumpareto.com>
2023-10-25 20:15:59 +0200

    fix some tasks service tests

--- backend/tests/services/task.service.tests.ts
+++ backend/tests/services/task.service.tests.ts
@@ -7,7 +7,7 @@ import {
   expect,
   it,
 } from "@jest/globals";
-
+import { interfaces } from "inversify-express-utils";
 import {
   dropTestCollections,
   dropTestDB,
@@ -15,13 +15,18 @@ import {
 } from "../db.testSetup.helpers";
 
 import { getTaskCollection } from "dbSchemas/task.schema";
+import { EventService } from "framework/events/event.service";
 import { IUserAttached } from "linked-models/user/user.model";
+import { ScheduleNotificationService } from "services/notification/schedule.notification.service";
 import { TaskService } from "services/task/task.service";
 import { ID_THAT_DOES_NOT_EXITS } from "../mocks/constants.mock";
 import { MOCKED_TASK_ID, mockedTask } from "../mocks/task.mock";
+import { MOCKED_USER_ID } from "../mocks/user.mock";
 
 describe(`Task service`, () => {
   let taskService: TaskService;
+  let eventService: EventService;
+  let scheduleNotificationService: ScheduleNotificationService;
 
   beforeAll(async () => {
     await setUpTestDB();
@@ -29,7 +34,14 @@ describe(`Task service`, () => {
 
   beforeEach(async () => {
     await getTaskCollection().create(mockedTask);
-    taskService = new TaskService(getTaskCollection());
+    eventService = new EventService({} as interfaces.HttpContext);
+    scheduleNotificationService = new ScheduleNotificationService();
+
+    taskService = new TaskService(
+      getTaskCollection(),
+      eventService,
+      scheduleNotificationService
+    );
   });
 
   afterEach(async () => {
@@ -71,7 +83,9 @@ describe(`Task service`, () => {
       text: "new task text",
     };
 
-    const task = await taskService.updateTask(MOCKED_TASK_ID, taskToUpdate);
+    const task = await taskService.updateTask(MOCKED_TASK_ID, taskToUpdate, {
+      id: MOCKED_USER_ID,
+    } as IUserAttached);
 
     expect(task?.text).toEqual(taskToUpdate.text);
   });
@@ -83,7 +97,9 @@ describe(`Task service`, () => {
 
     expect(
       async () =>
-        await taskService.updateTask(ID_THAT_DOES_NOT_EXITS, taskToUpdate)
+        await taskService.updateTask(ID_THAT_DOES_NOT_EXITS, taskToUpdate, {
+          id: MOCKED_USER_ID,
+        } as IUserAttached)
     ).rejects.toThrow(
       `Cannot update task: ${ID_THAT_DOES_NOT_EXITS}, because it does not exist.`
     );
@@ -94,7 +110,7 @@ describe(`Task service`, () => {
 
     expect(taskBeforeDelete?.text).toEqual("task1");
 
-    await taskService.deleteTask(MOCKED_TASK_ID);
+    await taskService.deleteTask(MOCKED_TASK_ID, MOCKED_USER_ID);
     const taskAfterDelete = await taskService.getTaskById(MOCKED_TASK_ID);
 
     expect(taskAfterDelete).toEqual(undefined);
@@ -102,7 +118,8 @@ describe(`Task service`, () => {
 
   it(`should throw proper error when trying to delete task that does not exist`, async () => {
     expect(
-      async () => await taskService.deleteTask(ID_THAT_DOES_NOT_EXITS)
+      async () =>
+        await taskService.deleteTask(ID_THAT_DOES_NOT_EXITS, MOCKED_USER_ID)
     ).rejects.toThrow(
       `Cannot delete task: ${ID_THAT_DOES_NOT_EXITS}, because it does not exist.`
     );
