091bf3d1e494b460dbc93e96c35a4e4a1a660a74
w.szutryk <w.szutryk@optimumpareto.com>
2023-11-13 10:53:51 +0100

    fix notifications spacing problems

--- frontend/src/pages/UserPage/components/NotificationsSettings/styles.ts
+++ frontend/src/pages/UserPage/components/NotificationsSettings/styles.ts
@@ -1,8 +1,9 @@
 import { styled } from "@mui/material";
+import { Button } from "atomicComponents/atoms/Button";
 
 export const StyledForm = styled("form")({
   display: "flex",
   flexDirection: "column",
-  justifyContent: 'space-between',
-  height: 200,
+  justifyContent: "space-between",
 });
+export const StyledSubmitButton = styled(Button)({ marginTop: 10 });
