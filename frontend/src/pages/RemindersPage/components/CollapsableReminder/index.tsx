b3f7ec55b5177f2726432986e65e556d755d9d99
w.szutryk <w.szutryk@optimumpareto.com>
2023-11-14 22:52:20 +0100

    agenda view styles fixes

--- frontend/src/pages/RemindersPage/components/CollapsableReminder/index.tsx
+++ frontend/src/pages/RemindersPage/components/CollapsableReminder/index.tsx
@@ -1,10 +1,12 @@
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { ListItemText } from "@mui/material";
 import { IReminderAttached } from "linked-models/reminder/reminder.model";
+import TaskDetailsList from "pages/SingleTodoListPage/components/TodoListCard/components/CardContent/components/TaskListItem/TaskDetailsList";
 import {
   StyledDetailsColapse,
   StyledListItemIcon,
 } from "pages/SingleTodoListPage/components/TodoListCard/components/CardContent/components/TaskListItem/styles";
-import TaskDetailsList from "pages/SingleTodoListPage/components/TodoListCard/components/CardContent/components/TaskListItem/TaskDetailsList";
+import { StyledExpandMore } from "pages/SingleTodoListPage/components/TodoListCard/styles";
 import TodoListIcon from "pages/TodoListsPage/components/TodoListIcon";
 import { memo, useState } from "react";
 import { StyledRemindersListItem } from "./styles";
@@ -15,12 +17,18 @@ interface Props {
 
 const CollapsableReminder = ({ reminder }: Props): JSX.Element => {
   const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = (e: React.MouseEvent<HTMLElement>) => {
+    e.stopPropagation();
+    setExpanded(!expanded);
+  };
+
   return (
     <StyledRemindersListItem
+      disableGutters
+      dense
+      disablePadding
       key={reminder.taskId}
-      onClick={() => {
-        setExpanded((prev) => !prev);
-      }}
     >
       {reminder.icon && (
         <StyledListItemIcon>
@@ -28,6 +36,15 @@ const CollapsableReminder = ({ reminder }: Props): JSX.Element => {
         </StyledListItemIcon>
       )}
       <ListItemText primary={reminder.text} />
+
+      <StyledExpandMore
+        expand={expanded}
+        onClick={handleExpandClick}
+        aria-expanded={expanded}
+        aria-label="show more"
+      >
+        <ExpandMoreIcon />
+      </StyledExpandMore>
       <StyledDetailsColapse in={expanded}>
         <TaskDetailsList
           task={{
--- frontend/src/pages/RemindersPage/components/CollapsableReminder/styles.ts
+++ frontend/src/pages/RemindersPage/components/CollapsableReminder/styles.ts
@@ -1,7 +1,7 @@
 import { ListItem, styled } from "@mui/material";
 
 export const StyledRemindersListItem = styled(ListItem)(({ theme }) => ({
-  color: theme.palette.secondary.contrastText,
+  color: theme.palette.primary.contrastText,
   flexWrap: "wrap",
   "& > li > ul > li:first-child": {
     borderTop: "none",
