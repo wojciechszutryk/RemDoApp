d0b673c8264359cd0d8aa7b06f64eaf6f35c6c84
w.szutryk <w.szutryk@optimumpareto.com>
2023-12-15 21:47:52 +0100

    add email service's sendEmailNotifications method

--- backend/src/services/notification/email.notification.service.ts
+++ backend/src/services/notification/email.notification.service.ts
@@ -1,30 +1,71 @@
 import { render } from "@react-email/render";
 import sendgrid, { MailService } from "@sendgrid/mail";
-import { injectable } from "inversify";
+import { inject, injectable } from "inversify";
+import { AppLanguages } from "linked-models/language/languages.enum";
+import { IUserAttached } from "linked-models/user/user.model";
+import { INotificationsTexts } from "models/notification.text.model";
+import React from "react";
+import { UserService } from "services/user/user.service";
+import EventTemplate from "../../../emails/EventTemplate";
 
 @injectable()
 export class EmailNotificationService {
   private readonly sendGridClient: MailService;
 
-  constructor() {
+  constructor(
+    @inject(UserService)
+    private readonly userService: UserService
+  ) {
     this.sendGridClient = sendgrid;
     this.sendGridClient.setApiKey(process.env.SENDGRID_API_KEY!);
   }
 
+  public async sendEmailNotifications(
+    notificationTexts: INotificationsTexts,
+    notificationLink: string | null,
+    usersToNotify: IUserAttached[],
+    eventCreatorImg: string | undefined
+  ) {
+    const emails = usersToNotify.map((user) => {
+      const userLang = user.preferences.language || AppLanguages.en;
+      const userTheme = user.preferences.theme || "light";
+
+      return {
+        from: "wscode@wscode.com.pl",
+        to: user.email,
+        subject: notificationTexts.title[AppLanguages.pl],
+        html: render(
+          React.createElement(EventTemplate, {
+            preview: notificationTexts.title[AppLanguages.pl],
+            body: notificationTexts.description[userLang],
+            link: notificationLink,
+            isDarkTheme: userTheme === "dark",
+            eventCreatorImg: eventCreatorImg,
+            userId: user.id,
+            unsubToken: user.preferences.emailUnsubscribeToken,
+          })
+        ),
+      };
+    });
+
+    await this.sendGridClient.send(emails);
+  }
+
   public async sendEmail(
-    email: string,
+    email: string[] | string,
     /** React.Element - template of email to send */
-    template: any
+    template: any,
+    subject: string
   ): Promise<void> {
     const emailHtml = render(template);
 
     const options = {
       from: "wscode@wscode.com.pl",
       to: email,
-      subject: "hello world",
+      subject: subject,
       html: emailHtml,
     };
 
-    this.sendGridClient.send(options);
+    await this.sendGridClient.send(options);
   }
 }
