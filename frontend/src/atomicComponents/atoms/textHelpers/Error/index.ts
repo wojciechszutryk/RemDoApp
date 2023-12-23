162ca2e421df78ac473d673703fa1b37d4448da5
w.szutryk <w.szutryk@optimumpareto.com>
2023-12-23 17:12:03 +0100

    clean unused atomic components

--- frontend/src/atomicComponents/atoms/textHelpers/Error/index.ts
+++
@@ -1,7 +0,0 @@
-import { styled, Typography } from "@mui/material";
-
-export const ErrorText = styled(Typography)(({ theme }) => ({
-  fontSize: "12px",
-  color: theme.palette.warning.main,
-  margin: 0,
-}));
