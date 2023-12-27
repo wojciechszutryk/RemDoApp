101dd505b42172be4d16b9f46af9a4d723e942a0
w.szutryk <w.szutryk@optimumpareto.com>
2023-12-27 12:06:07 +0100

    add mutations for pass recovery

---
+++ frontend/src/pages/SharedPages/ForgetPasswordPage/useSetForgottenPassword.mutation.ts
@@ -0,0 +1,34 @@
+import { useMutation } from "@tanstack/react-query";
+import { apiPost } from "framework/asyncInteractions";
+import { FRONTIFY_URL } from "framework/asyncInteractions/frontifyRequestUrl.helper";
+import {
+  URL_FORGET_PASSWORD,
+  URL_USER,
+  URL_USERS,
+} from "linked-models/user/user.urls";
+
+export const useSetForgottenPassword = () => {
+  const forgetPassword = async ({
+    userId,
+    newPassword,
+  }: {
+    userId: string;
+    newPassword: string;
+  }): Promise<void> => {
+    const url = FRONTIFY_URL(
+      URL_FORGET_PASSWORD,
+      `${URL_USERS}${URL_USER(userId)}`
+    );
+    await apiPost<{ newPassword: string }, void>(
+      url,
+      { newPassword },
+      {
+        headers: {
+          "custom-token": "header",
+        },
+      }
+    );
+  };
+
+  return useMutation(forgetPassword);
+};
