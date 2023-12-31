4fefd1490703d3968e2e9f755683dc11f7cc167b
w.szutryk <w.szutryk@optimumpareto.com>
2023-12-31 17:30:35 +0100

    make accordion more reusable

--- frontend/src/atomicComponents/atoms/Accordion/index.tsx
+++ frontend/src/atomicComponents/atoms/Accordion/index.tsx
@@ -1,14 +1,14 @@
-import { AccordionDetails, AccordionSummary, Typography } from "@mui/material";
+import { AccordionDetails, AccordionSummary, Typography, AccordionProps } from "@mui/material";
 import { StyledAccordion, StyledExpandMoreIcon } from "./styles";
 
-interface Props {
+interface Props extends AccordionProps {
   summaryText: string;
   children: JSX.Element | JSX.Element[];
 }
 
-const Accordion = ({ summaryText, children }: Props): JSX.Element => {
+const Accordion = ({ summaryText, children, ...rest }: Props): JSX.Element => {
   return (
-    <StyledAccordion>
+    <StyledAccordion {...rest}>
       <AccordionSummary expandIcon={<StyledExpandMoreIcon />}>
         <Typography> {summaryText}</Typography>
       </AccordionSummary>
