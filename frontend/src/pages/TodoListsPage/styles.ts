c885aecd77c305532b7d90bf99d1f9415f9b8ad5
w.szutryk <w.szutryk@optimumpareto.com>
2024-02-24 12:28:06 +0100

    change todolists layout

--- frontend/src/pages/TodoListsPage/styles.ts
+++ frontend/src/pages/TodoListsPage/styles.ts
@@ -10,14 +10,87 @@ export const StyledTodoListsPageWrapper = styled("main")(({ theme }) => ({
   },
 }));
 
-export const StyledTodoListsWrapper = styled("section", {
-  shouldForwardProp: (prop) => prop !== "columns",
-})<{ columns: number }>(({ columns }) => ({
-  display: "grid",
-  gridTemplateColumns: `repeat(${columns}, 1fr)`,
-  gridGap: 20,
+//columnCount approach
+// export const StyledTodoListsWrapper = styled("section", {
+//   shouldForwardProp: (prop) => prop !== "columns",
+// })<{ columns: number }>(({ columns }) => ({
+//   // display: "grid",
+//   // gridTemplateColumns: `repeat(${columns}, 1fr)`,
+//   // gridGap: 20,
+//   columnCount: columns,
+//   maxWidth: "1000px",
+//   margin: "50px auto",
+//   "& > div": {
+//     marginBottom: 17,
+//     "& > div": {
+//       height: "100%",
+//       display: "flex",
+//       flexDirection: "column",
+//       "& > div:nth-child(2)": {
+//         height: "100%",
+//       },
+//     },
+//   },
+// }));
+
+//grid approach
+// })<{ columns: number }>(({ columns }) => ({
+//   display: "grid",
+//   gridTemplateColumns: `repeat(${columns}, 1fr)`,
+//   gridGap: 20,
+//   maxWidth: "1000px",
+//   margin: "50px auto",
+//   "& > div > div": {
+//     height: "100%",
+//     display: "flex",
+//     flexDirection: "column",
+//     "& > div:nth-child(2)": {
+//       height: "100%",
+//     },
+//   },
+// }));
+
+//flexbox approach
+// export const StyledListsColWrapper = styled("section", {
+//   shouldForwardProp: (prop) => prop !== "columns",
+// })<{ columns: number }>(({ columns }) => ({
+// display: "flex",
+// flexDirection: "column",
+// flexWrap: "wrap",
+// height: "200vh",
+//   // display: "grid",
+//   // gridTemplateColumns: `repeat(${columns}, 1fr)`,
+//   gridGap: 20,
+//   maxWidth: "1000px",
+//   margin: "50px auto",
+//   "& > div": {
+//     // marginBottom: 17,
+//     width: "50%",
+//     "& > div": {
+//       height: "100%",
+//       display: "flex",
+//       flexDirection: "column",
+//       "& > div:nth-child(2)": {
+//         height: "100%",
+//       },
+//     },
+//   },
+// }));
+
+export const StyledListsColWrapper = styled("section")({
+  display: "flex",
   maxWidth: "1000px",
   margin: "50px auto",
+  gap: 20,
+});
+
+export const StyledListsCol = styled("section", {
+  shouldForwardProp: (prop) => prop !== "columns",
+})<{ columns: number }>(({ columns }) => ({
+  display: "flex",
+  flexDirection: "column",
+  gap: 20,
+  width: `calc(${100 / columns}% - 20px)`,
   "& > div > div": {
     height: "100%",
     display: "flex",
