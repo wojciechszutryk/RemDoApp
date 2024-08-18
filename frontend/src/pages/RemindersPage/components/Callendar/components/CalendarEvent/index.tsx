1885068c48917c59c7b3593d08b2c41058dfbf4d
w.szutryk <w.szutryk@optimumpareto.com>
2024-08-18 22:17:01 +0200

    remove icons from reminders

--- frontend/src/pages/RemindersPage/components/Callendar/components/CalendarEvent/index.tsx
+++ frontend/src/pages/RemindersPage/components/Callendar/components/CalendarEvent/index.tsx
@@ -1,21 +1,14 @@
 import { ICallendarEvent } from "pages/RemindersPage/helpers/models";
 import { memo } from "react";
 import { EventProps } from "react-big-calendar";
-import { StyledEventWrapper, StyledTodoListIcon } from "./styles";
+import { StyledEventWrapper } from "./styles";
 
 interface Props {
   calendarEvent: EventProps<ICallendarEvent>;
 }
 
 const CallendarEvent = ({ calendarEvent }: Props): JSX.Element => {
-  return (
-    <StyledEventWrapper>
-      {calendarEvent.event.icon && (
-        <StyledTodoListIcon type={calendarEvent.event.icon} />
-      )}
-      {calendarEvent.event.name}
-    </StyledEventWrapper>
-  );
+  return <StyledEventWrapper>{calendarEvent.event.name}</StyledEventWrapper>;
 };
 
 export default memo(CallendarEvent);
