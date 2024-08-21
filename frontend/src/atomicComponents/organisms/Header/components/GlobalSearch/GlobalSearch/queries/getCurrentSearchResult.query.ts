70dea629dcfcbcfc7b193410fea11264491446cf
w.szutryk <w.szutryk@optimumpareto.com>
2024-08-21 10:48:23 +0200

    add search queries

---
+++ frontend/src/atomicComponents/organisms/Header/components/GlobalSearch/GlobalSearch/queries/getCurrentSearchResult.query.ts
@@ -0,0 +1,27 @@
+import {
+  useQuery,
+  UseQueryOptions,
+  UseQueryResult,
+} from "@tanstack/react-query";
+import { apiGet } from "framework/asyncInteractions";
+import { FRONTIFY_URL } from "framework/asyncInteractions/frontifyRequestUrl.helper";
+import { ISearchResults } from "linked-models/search/search.model";
+import {
+  SEARCH_PHRASE_PARAM,
+  URL_SEARCH,
+} from "linked-models/search/search.urls";
+
+export const useGetSearchResultQuery = (
+  searchQuery: string,
+  options?: Omit<UseQueryOptions<ISearchResults>, "queryFn">
+): UseQueryResult<ISearchResults, unknown> => {
+  const url = FRONTIFY_URL(URL_SEARCH, undefined, {
+    [SEARCH_PHRASE_PARAM]: searchQuery,
+  });
+
+  const getCurrentSearchResult = async () => {
+    return await apiGet<ISearchResults>(url).then((res) => res.data);
+  };
+
+  return useQuery([URL_SEARCH, searchQuery], getCurrentSearchResult, options);
+};
