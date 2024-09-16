39a211343b56827ef429f52ec9dd769e516ce617
w.szutryk <w.szutryk@optimumpareto.com>
2024-09-16 18:02:28 +0200

    simplify textfield

--- frontend/src/atomicComponents/atoms/TextField/index.tsx
+++ frontend/src/atomicComponents/atoms/TextField/index.tsx
@@ -3,24 +3,13 @@ import { forwardRef } from "react";
 import AnimatedWaveAlt from "../AnimatedWaveAlt";
 import { StyledTextField } from "./styles";
 
-interface Props extends Omit<TextFieldProps<"outlined">, "variant"> {
-  noBorder?: boolean;
-}
-
-const TextField = forwardRef((props: Props, ref): JSX.Element => {
+const TextField = forwardRef((props: TextFieldProps, ref): JSX.Element => {
   return (
     <StyledTextField
       {...props}
       InputProps={{
         ...props.InputProps,
-        endAdornment: props?.InputProps?.endAdornment ? (
-          <>
-            {props.InputProps.endAdornment}
-            <AnimatedWaveAlt />
-          </>
-        ) : (
-          <AnimatedWaveAlt />
-        ),
+        endAdornment: <AnimatedWaveAlt />,
         ref: ref,
       }}
     />
