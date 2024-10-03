3d88275b0ee139411b4cecb3c92cc421adfd7313
w.szutryk <w.szutryk@optimumpareto.com>
2024-10-03 21:12:46 +0200

    refactor GlobalSearch component

--- frontend/src/atomicComponents/organisms/Header/components/GlobalSearch/index.tsx
+++ frontend/src/atomicComponents/organisms/Header/components/GlobalSearch/index.tsx
@@ -2,10 +2,8 @@ import SearchIcon from "@mui/icons-material/Search";
 import { Drawer } from "@mui/material";
 import { memo, useState } from "react";
 import { StyledHeaderIconButton } from "../../styles";
-import EmptyNotificationsInfo from "../NotificationsMenu/components/EmptyNotificationsInfo";
-import NotificationsLoader from "../NotificationsMenu/components/NotificationsLoader";
 import useToggleDrawer from "../NotificationsMenu/hooks/useToggleDrawer";
-import { StyledDrawerListWrapper } from "./styles";
+import Search from "./Search";
 
 const GlobalSearch = (): JSX.Element => {
   const [showSearchDrawer, setShowSearchDrawer] = useState(false);
@@ -26,19 +24,14 @@ const GlobalSearch = (): JSX.Element => {
             margin: "10px auto 50px",
             height: "90vh",
             maxWidth: 800,
+            borderRadius: 10,
           },
         }}
         open={showSearchDrawer}
         onClose={toggleDrawer(false)}
         anchor="top"
       >
-        <StyledDrawerListWrapper
-          role="presentation"
-          onKeyDown={toggleDrawer(false)}
-        >
-          <NotificationsLoader />
-          {<EmptyNotificationsInfo />}
-        </StyledDrawerListWrapper>
+        <Search />
       </Drawer>
     </>
   );
