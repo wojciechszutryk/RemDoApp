810c45ca38946846c7286cbe2d481a05d6bfc7b8
w.szutryk <w.szutryk@optimumpareto.com>
2024-11-11 10:22:06 +0100

    lazy load search

--- frontend/src/atomicComponents/organisms/Header/components/GlobalSearch/index.tsx
+++ frontend/src/atomicComponents/organisms/Header/components/GlobalSearch/index.tsx
@@ -1,9 +1,10 @@
 import SearchIcon from "@mui/icons-material/Search";
 import { Drawer } from "@mui/material";
-import { memo, useState } from "react";
+import { Suspense, lazy, memo, useState } from "react";
 import { StyledHeaderIconButton } from "../../styles";
 import useToggleDrawer from "../NotificationsMenu/hooks/useToggleDrawer";
-import Search from "./Search";
+
+const Search = lazy(() => import("./Search"));
 
 const GlobalSearch = (): JSX.Element => {
   const [showSearchDrawer, setShowSearchDrawer] = useState(false);
@@ -21,17 +22,20 @@ const GlobalSearch = (): JSX.Element => {
         PaperProps={{
           sx: {
             width: "90vw",
-            margin: "10px auto 50px",
+            margin: "0 auto 50px",
             height: "90vh",
             maxWidth: 800,
-            borderRadius: 10,
+            borderBottomLeftRadius: 20,
+            borderBottomRightRadius: 20,
           },
         }}
         open={showSearchDrawer}
         onClose={toggleDrawer(false)}
         anchor="top"
       >
-        <Search />
+        <Suspense fallback={null}>
+          <Search />
+        </Suspense>
       </Drawer>
     </>
   );
