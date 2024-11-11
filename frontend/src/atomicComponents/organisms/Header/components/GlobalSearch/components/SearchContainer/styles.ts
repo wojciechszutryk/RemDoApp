9190a8517dbebc4dfa872069a9b223c4a1e32a32
w.szutryk <w.szutryk@optimumpareto.com>
2024-11-11 12:35:39 +0100

    search cleanup

--- frontend/src/atomicComponents/organisms/Header/components/GlobalSearch/components/SearchContainer/styles.ts
+++ frontend/src/atomicComponents/organisms/Header/components/GlobalSearch/components/SearchContainer/styles.ts
@@ -1,19 +1,9 @@
-import { Box, styled } from "@mui/material";
+import { styled } from "@mui/material";
 
-export const StyledSearchContainerWrapper = styled(Box)(() => ({
-  zIndex: 1300,
+export const StyledSearchContainerWrapper = styled("div")(() => ({
   width: "100%",
 }));
 
-export const SearchResultsWrapper = styled(Box)({
-  zIndex: 1300,
-  overflow: "auto",
-  maxHeight: "calc(100vh - 148px)",
-  "&::-webkit-scrollbar-track": {
-    marginBottom: "2px",
-    borderRadius: "0 0 8px 0px",
-  },
-  "@media (max-width: 1024px)": {
-    maxHeight: "unset",
-  },
+export const SearchResultsWrapper = styled("div")({
+  padding: "10px 5%",
 });
