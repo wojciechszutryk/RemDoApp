14edba29e4dd293d271ea7e2913c9c596688ff73
w.szutryk <w.szutryk@optimumpareto.com>
2024-11-30 11:08:25 +0100

    todo card title refactor

--- frontend/src/pages/SingleTodoListPage/components/TodoListCard/components/CardHeader/styles.ts
+++ frontend/src/pages/SingleTodoListPage/components/TodoListCard/components/CardHeader/styles.ts
@@ -1,17 +1,28 @@
 import { AvatarGroup, Typography, styled } from "@mui/material";
 
-export const StyledHeaderTitle = styled(Typography, {
-  shouldForwardProp: (prop) => prop !== "disableHover",
-})<{ disableHover?: boolean }>(({ theme, disableHover }) => ({
-  cursor: disableHover ? "unset" : "pointer",
+export const StyledHeaderTitle = styled(Typography)(({ theme }) => ({
+  cursor: "pointer",
   transition: "color 0.15s ease-in-out",
   "&:hover": {
-    color: disableHover ? "unset" : theme.palette.primary.main,
+    color: theme.palette.secondary.contrastText,
   },
 }));
 
 export const StyledAvatarGroup = styled(AvatarGroup)(({ theme }) => ({
   "& .MuiAvatar-root": {
     borderColor: theme.palette.background.paper,
+  },
+}));
+
+export const CollapseHandlersWrapper = styled("div")(({ theme }) => ({
+  position: "relative",
+  display: "inline-block",
+  width: 20,
+  "& svg": {
+    cursor: "pointer",
+    transition: "color 0.15s ease-in-out",
+    "&:hover": {
+      path: theme.palette.secondary.contrastText,
+    },
   },
 }));
